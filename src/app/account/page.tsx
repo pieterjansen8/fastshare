8976afaf080c0cb6586dd084680b9c07074fa725
pieterjansen8 <pieterjansen8@icloud.com>
2025-01-17 23:38:55 +0100

    1.2

---
+++ src/app/account/page.tsx
@@ -0,0 +1,23 @@
+import { AccountSettings } from '@stackframe/stack';
+import  Link  from 'next/link';
+export default function MyAccountPage() {
+  return (
+    <AccountSettings
+      fullPage={true}
+      extraItems={[{
+        title: 'Go back',
+        iconName: "ArrowLeft",
+        content: <CustomContent />,
+        id: 'go-back',
+      }]}
+    />
+  );
+}
+function CustomContent(){
+    return(
+         <Link href={"/"}>
+              <span  className='text-blue-600  underline text-2xl'>Go back</span>
+          </Link>
+    )
+   
+}
--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,6 +1,7 @@
-import { StackHandler } from "@stackframe/stack";
+import { AccountSettings, StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "../../../stack";
+import { title } from "process";
 
 export default function Handler(props: unknown) {
-  return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
+  return <StackHandler fullPage app={stackServerApp} routeProps={props}></StackHandler>
 }
